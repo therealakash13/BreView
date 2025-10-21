8687b8c93be8b2daf18fe55c6f5be456eaa60cf3
Akash <therealakash13@gmail.com>
2025-10-21 14:49:52 +0530

    pagination done

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ server.set("view engine", "ejs");
 server.use(express.static("public"));
 server.use(bodyParser.urlencoded({ extended: true }));
 
-async function searchBook(str) {
+async function searchBook(str,page) {
   try {
     const response = await axios.get(
       "https://www.googleapis.com/books/v1/volumes",
@@ -34,10 +34,11 @@ async function searchBook(str) {
           maxResults: 10,
           printType: "books",
           projection: "full",
+          startIndex: page * 10,
         },
       }
     );
-    console.log(response.data);
+  
     // Todo: Paginate using startIndex and maxResults
     return response.data.items;
   } catch (error) {
@@ -54,16 +55,16 @@ server.get("/", async (req, res) => {
   res.render("index", { title: "BreView", books: books });
 });
 
-server.get("/search", async (req, res) => {
-  res.render("search", { results: [], q: "" });
-});
+server.get('/search',async(req,res)=>{
+  const query = req.query.q || '';
+  const page = Number(req.query.page) || 0;
+  const books = await searchBook(query,page);
 
-server.post("/search", async (req, res) => {
-  const searchString = req.body.q;
-  const bookRes = await searchBook(searchString);
-
-  res.render("search", { results: bookRes, q: searchString });
-});
+  if (books.length === 0) {
+    return res.render('search',{q:query,page:page, results:[]})
+  }
+  res.render('search', {q:query,page:page, results:books });
+})
 
 server.listen(port, (err) => {
   if (err) console.log("Error while starting server :", err);
