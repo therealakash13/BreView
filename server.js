809e4b4734674d281f598265dfac131b90277aa2
Akash <therealakash13@gmail.com>
2025-10-22 14:03:41 +0530

    review and book crud

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ async function searchBook(str, page) {
 }
 
 server.get("/", async (req, res) => {
-  const user = await pool.query(`SELECT * FROM users`);
-  const users = user.rows;
+  // const user = await pool.query(`SELECT * FROM users`);
+  // const users = user.rows;
   const book = await pool.query(`SELECT * FROM books`);
   const books = book.rows;
 
@@ -66,46 +66,96 @@ server.get("/search", async (req, res) => {
 });
 
 server.get("/books/add/:id", async (req, res) => {
-  const bookId = req.params.id;
-  console.log({ bookId });
+  try {
+    const bookId = req.params.id;
 
-  const response = await axios.get(
-    `https://www.googleapis.com/books/v1/volumes/${bookId}`,
-    {
-      params: { key: process.env.API_KEY },
-    }
-  );
-  const bookData = response.data;
-  const book = bookData;
+    if (bookId === "" || bookId === undefined || bookId === null)
+      throw new Error("Book id not found.");
 
-  await pool.query(
-    `
-  INSERT INTO books (
-    gb_id, title, author_names, categories, pub_date, description, pg_count,
-    book_type, isbn_13, m_rating, thumbnail, language, g_link
-  )
-  VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)
-  ON CONFLICT (g_link) DO NOTHING
-  `,
-    [
-      book.id,
-      book.volumeInfo.title || "Untitled",
-      book.volumeInfo.authors || [],
-      book.volumeInfo.categories || [],
-      book.volumeInfo.publishedDate || null,
-      book.volumeInfo.description || null,
-      book.volumeInfo.pageCount || null,
-      book.volumeInfo.printType || null,
-      book.volumeInfo.industryIdentifiers?.find((i) => i.type === "ISBN_13")
-        ?.identifier || null,
-      book.volumeInfo.averageRating || null,
-      book.volumeInfo.imageLinks?.thumbnail || null,
-      book.volumeInfo.language || "en",
-      book.volumeInfo.infoLink || null,
-      // Fix some values are off
-    ]
-  );
+    const response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes/${bookId}`,
+      {
+        params: { key: process.env.API_KEY, projection: "lite" },
+      }
+    );
 
+    if (!response) throw new Error("Book not found.");
+
+    const bookData = response.data;
+    // console.log(bookData);
+
+    const book = {
+      id: bookData.id,
+      title: bookData.volumeInfo.title,
+      authors: bookData.volumeInfo.authors,
+      publishDate: bookData.volumeInfo.publishedDate,
+      description: bookData.volumeInfo.description,
+      bookCover:
+        bookData.volumeInfo.imageLinks.extraLarge ||
+        bookData.volumeInfo.imageLinks.large ||
+        bookData.volumeInfo.imageLinks.medium ||
+        bookData.volumeInfo.imageLinks.small ||
+        bookData.volumeInfo.imageLinks.thumbnail ||
+        bookData.volumeInfo.imageLinks.smallThumbnail,
+    };
+
+    console.log(book);
+
+    await pool.query(
+      `
+    INSERT INTO books (
+      book_id, image, title, author_name, publish_date, description
+    )
+    VALUES ($1,$2,$3,$4,$5,$6)
+    `,
+      [
+        book.id,
+        book.bookCover,
+        book.title,
+        book.authors,
+        book.publishDate,
+        book.description,
+      ]
+    );
+
+    res.redirect("/");
+  } catch (err) {
+    console.log(err.message || err.code);
+  }
+});
+
+server.get("/review/:id", async (req, res) => {
+  const bookId = Number(req.params.id);
+  const book = await pool.query(`SELECT * FROM books WHERE id = ${bookId}`);
+  res.render("review", { book: book.rows[0] });
+});
+
+server.post("/review/:id", async (req, res) => {
+  try {
+    const rating = req.body.rating;
+    const review = req.body.review;
+    const id = req.params.id;
+
+    if (review === "" || !id)
+      throw new Error("insufficient information provided.");
+
+    await pool.query(
+      `
+      UPDATE books
+      SET review = $1, rating = $2
+      WHERE id = $3;
+      `,
+      [review, rating, id]
+    );
+
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+server.get("/remove/:id", async (req, res) => {
+  await pool.query(`DELETE FROM books WHERE id = $1`, [req.params.id]);
   res.redirect("/");
 });
 
--- tables.sql
+++ tables.sql
@@ -1,48 +1,20 @@
 -- USERS TABLE
-CREATE TABLE users (
-    id                  SERIAL              PRIMARY KEY,
-    name                VARCHAR(50)         UNIQUE                      NOT NULL,
-    accent              TEXT
-);
-
--- AUTHORS TABLE
-CREATE TABLE authors (
-    id                  SERIAL              PRIMARY KEY,
-    name                VARCHAR(100)        NOT NULL,
-    key                 TEXT,               
-    top_work            TEXT,
-    work_count          INTEGER
-);
+-- CREATE TABLE users (
+--     id                  SERIAL              PRIMARY KEY,
+--     name                VARCHAR(50)         UNIQUE                      NOT NULL,
+--     accent              TEXT
+-- );
 
 -- BOOKS TABLE
 CREATE TABLE books (
-    id                  SERIAL              PRIMARY KEY,
-    user_id             INTEGER             REFERENCES users(id)        ON DELETE SET NULL,
-    author_id           INTEGER             REFERENCES authors(id)      ON DELETE SET NULL,
-    gb_id               TEXT,                    
-    title               TEXT                NOT NULL,
-    author_names        TEXT[],
-    categories          TEXT[]                   
-    pub_date            VARCHAR(10),
-    description         TEXT,
-    pg_count            INTEGER,
-    book_type           VARCHAR(50),
-    isbn_13             VARCHAR(13),
-    m_rating            TEXT,
-    thumbnail           TEXT,
-    language            CHAR(3),
-    g_link              TEXT                 UNIQUE                
-);
-
--- USER REVIEWS
-CREATE TABLE reviews (
-    id                  SERIAL                PRIMARY KEY,
-    user_id             INTEGER               REFERENCES users(id) ON DELETE CASCADE,
-    book_id             INTEGER               REFERENCES books(id) ON DELETE CASCADE,
-    rating              NUMERIC(2,1)          CHECK (rating BETWEEN 0 AND 5),
-    review              TEXT,
-    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (user_id,book_id)
+    id              SERIAL PRIMARY KEY,
+    image           TEXT,
+    title           TEXT NOT NULL,
+    author_name     VARCHAR(45)[],
+    publish_date    TEXT,
+    description     TEXT,
+    rating          CHAR(10),
+    review          TEXT
 );
 
 -- TODO Fix the tables and create db according to the Googles Books API
